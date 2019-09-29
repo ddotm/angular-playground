9d80ec81174b277792b72ba1c1bb5f025611b0a4
Dmitri Mogilevski <dmogilevski@gmail.com>
2019-09-29 15:19:40 -0500

    npm install ag-grid, added it to module

--- src/app/grid/grid.module.ts
+++ src/app/grid/grid.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {AgGridModule} from 'ag-grid-angular';
 import {GridComponent} from './grid/grid.component';
 
 @NgModule({
@@ -7,7 +8,8 @@ import {GridComponent} from './grid/grid.component';
     GridComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AgGridModule.withComponents([])
   ]
 })
 export class GridModule {
